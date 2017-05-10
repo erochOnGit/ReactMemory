66d4cc45a9000df28c81d6ada6db371552167aae
eroch <eroch@pros.com>
2017-05-10 14:58:53 +0200

    .eroch i try to be a good  tdd'er

--- src/tests/memory.test.js
+++ src/tests/memory.test.js
@@ -3,21 +3,27 @@ function Memory(tabTiles){
   this.tabTilesObject = [];
 
   for(var i =0;i<this.tabTiles.length;i++) {
+      let tab = this.tabTilesObject;
     this.tabTilesObject.push({
       name:this.tabTiles[i],
       state:"dead",
     flip:function(){
-      this.state="alive"
+        for(var i = 0; i<tab.length;i++){
+            if(tab[i].state=="alive"){
+                if(tab[i].name!=this.name){
+                    tab[i].state="dead"
+                    this.state="dead"
+                }
+                else{}
+          }else{
+              this.state="alive"
+          }
+        }
     }
     })
   }
 }
-
 Memory.prototype.display=function(){
-  return this.tabTilesObject
-}
-
-Memory.prototype.flip=function(){
   return this.tabTilesObject
 }
 
@@ -49,4 +55,10 @@ test('state should be alive when flipped twice',function(){
 
 })
 
+test('state should be dead when I flipped two different tiles',function(){
+    let jeu = new Memory(['lune','soleil','lune','soleil'])
+    jeu.tabTilesObject[0].flip();
+    jeu.tabTilesObject[1].flip();
+    expect(jeu.display()[0].state).toEqual("dead")
 
+})
