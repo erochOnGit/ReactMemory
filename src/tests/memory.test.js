d864a0d1460da0446cf3895e785cb67b963f467b
eroch <eroch@pros.com>
2017-05-11 17:30:32 +0200

    .eroch remove tiles when I make an even

--- src/tests/memory.test.js
+++ src/tests/memory.test.js
@@ -1,57 +1,70 @@
 function Memory(tabTiles){
-  this.tabTiles=tabTiles;
-  this.tabTilesObject = [];
-
-  for(var i =0;i<this.tabTiles.length;i++) {
-      let tab = this.tabTilesObject;
-    this.tabTilesObject.push({
-      name:this.tabTiles[i],
-      state:"dead",
-    flip:function(){
-        for(var i = 0; i<tab.length;i++){
-            if(tab[i].state=="alive"){
-                if(tab[i].name!=this.name){
-                    tab[i].state="dead"
-                    this.state="dead"
+    this.tabTiles=tabTiles;
+    this.tabTilesObject = [];
+    this.tabTilesOwned = [];
+    for(var i =0;i<this.tabTiles.length;i++) {
+        let tabObject = this.tabTilesObject;
+        let tabOwned = this.tabTilesOwned;
+        let jeu = this;
+        this.tabTilesObject.push({
+            name:this.tabTiles[i],
+            state:"dead",
+            owned:function(){
+                for(var i = 0; i<tabObject.length;i++) {
+                    tabOwned.push(this)
+                    if (tabObject[i].name==this.name&&this!=tabObject[i]) {
+                        var index = tabObject.indexOf(this);
+                        tabObject.splice(index,1)
+                    }
                 }
-                else{}
-          }else{
-              this.state="alive"
-          }
-        }
+            },
+            flip:function(){
+                for(var i = 0; i<tabObject.length;i++){
+                    if(tabObject[i].state=="alive"&&this!=tabObject[i]){
+                        if(tabObject[i].name!=this.name){
+                            tabObject[i].state="dead"
+                            this.state="dead"
+                        }
+                        else{
+                            tabObject[i].owned();
+                            this.owned();
+                        }
+                    }else{
+                        this.state="alive"
+                    }
+                }
+            }
+        })
     }
-    })
-  }
 }
+
 Memory.prototype.display=function(){
-  return this.tabTilesObject
+    return this.tabTilesObject
 }
 
 test('shouldHave 4 tiles',function(){
-  let jeu = new Memory(['lune','soleil','lune','soleil'])
-  expect(jeu.display().length).toEqual(4)
-
+    let jeu = new Memory(['lune','soleil','lune','soleil'])
+    expect(jeu.display().length).toEqual(4)
 })
 
 test('Tile should be dead',function(){
-  let jeu = new Memory(['lune','soleil','lune','soleil'])
-
-  expect(jeu.display()[0].state).toEqual("dead")
+    let jeu = new Memory(['lune','soleil','lune','soleil'])
+    expect(jeu.display()[0].state).toEqual("dead")
 
 })
 
 test('state should be alive when flipped once',function(){
-  let jeu = new Memory(['lune','soleil','lune','soleil'])
-  jeu.tabTilesObject[0].flip();
-  expect(jeu.display()[0].state).toEqual("alive")
+    let jeu = new Memory(['lune','soleil','lune','soleil'])
+    jeu.tabTilesObject[0].flip();
+    expect(jeu.display()[0].state).toEqual("alive")
 
 })
 
 test('state should be alive when flipped twice',function(){
-  let jeu = new Memory(['lune','soleil','lune','soleil'])
-  jeu.tabTilesObject[0].flip();
-  jeu.tabTilesObject[0].flip();
-  expect(jeu.display()[0].state).toEqual("alive")
+    let jeu = new Memory(['lune','soleil','lune','soleil'])
+    jeu.tabTilesObject[0].flip();
+    jeu.tabTilesObject[0].flip();
+    expect(jeu.display()[0].state).toEqual("alive")
 
 })
 
@@ -61,4 +74,11 @@ test('state should be dead when I flipped two different tiles',function(){
     jeu.tabTilesObject[1].flip();
     expect(jeu.display()[0].state).toEqual("dead")
 
+})
+
+test('should have 2 tiles left when I flipped two same tiles',function(){
+    let jeu = new Memory(['lune','soleil','lune','soleil'])
+    jeu.tabTilesObject[0].flip();
+    jeu.tabTilesObject[2].flip();
+    console.log(jeu.display());
 })
