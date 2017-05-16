a0657f20710dc31429491e4f8f9fc4fee59f98d8
eroch <eroch@pros.com>
2017-05-16 10:11:11 +0200

    .eroch replace var  jeu by var game

--- src/tests/memory.test.js
+++ src/tests/memory.test.js
@@ -6,13 +6,13 @@ function Memory(tabTiles){
     for(var i =0;i<this.tabTiles.length;i++) {
         let tabObject = this.tabTilesObject;
         let tabOwned = this.tabTilesOwned;
-        let jeu = this;
+        let game = this;
         this.tabTilesObject.push({
             name:this.tabTiles[i],
             state:"dead",
             owned:function(){
                 for(var i = 0; i<tabObject.length;i++) {
-                    this.owner = jeu.currentPlayer;
+                    this.owner = game.currentPlayer;
                     if (tabObject[i].name==this.name) {
                         tabOwned.push(this);
                         var index = tabObject.indexOf(this);
@@ -36,10 +36,10 @@ function Memory(tabTiles){
                             foundTile = true;
                         }
                         if (foundTile) {
-                            if(jeu.currentPlayer=="player1"){
-                                jeu.currentPlayer="player2"
+                            if(game.currentPlayer=="player1"){
+                                game.currentPlayer="player2"
                             }else{
-                                jeu.currentPlayer="player1"
+                                game.currentPlayer="player1"
                             }
                             break;
                         }
@@ -69,71 +69,73 @@ Memory.prototype.getCurrentPlayer=function(){
 };
 
 test('shouldHave 4 tiles',function(){
-    let jeu = new Memory(['lune','soleil','lune','soleil']);
-    expect(jeu.display().length).toEqual(4)
+    let game = new Memory(['lune','soleil','lune','soleil']);
+    expect(game.display().length).toEqual(4)
 });
 
 test('Tile should be dead',function(){
-    let jeu = new Memory(['lune','soleil','lune','soleil']);
-    expect(jeu.display()[0].state).toEqual("dead")
+    let game = new Memory(['lune','soleil','lune','soleil']);
+    expect(game.display()[0].state).toEqual("dead")
 
 });
 
 test('state should be alive when flipped once',function(){
-    let jeu = new Memory(['lune','soleil','lune','soleil']);
-    jeu.tabTilesObject[0].flip();
-    expect(jeu.display()[0].state).toEqual("alive")
+    let game = new Memory(['lune','soleil','lune','soleil']);
+    game.tabTilesObject[0].flip();
+    expect(game.display()[0].state).toEqual("alive")
 
 });
 
 test('state should be alive when flipped twice',function(){
-    let jeu = new Memory(['lune','soleil','lune','soleil']);
-    jeu.tabTilesObject[0].flip();
-    jeu.tabTilesObject[0].flip();
-    expect(jeu.display()[0].state).toEqual("alive")
+    let game = new Memory(['lune','soleil','lune','soleil']);
+    game.tabTilesObject[0].flip();
+    game.tabTilesObject[0].flip();
+    expect(game.display()[0].state).toEqual("alive")
 
 });
 
 test('state should be dead when I flipped two different tiles',function(){
-    let jeu = new Memory(['lune','soleil','lune','soleil']);
-    jeu.tabTilesObject[0].flip();
-    jeu.tabTilesObject[1].flip();
-    expect(jeu.display()[0].state).toEqual("dead")
+    let game = new Memory(['lune','soleil','lune','soleil']);
+    game.tabTilesObject[0].flip();
+    game.tabTilesObject[1].flip();
+    expect(game.display()[0].state).toEqual("dead")
 
 });
 
 test('should have 2 tiles left when I flipped two same tiles',function(){
-    let jeu = new Memory(['lune','soleil','lune','soleil']);
-    jeu.tabTilesObject[0].flip();
-    jeu.tabTilesObject[2].flip();
-    expect(jeu.display().length).toEqual(2)
+    let game = new Memory(['lune','soleil','lune','soleil']);
+    game.tabTilesObject[0].flip();
+    game.tabTilesObject[2].flip();
+    expect(game.display().length).toEqual(2)
 });
 
 test('get current player',function(){
-    let jeu = new Memory(['lune','soleil','lune','soleil']);
-    expect(jeu.getCurrentPlayer()).toEqual("player1")
+    let game = new Memory(['lune','soleil','lune','soleil']);
+    expect(game.getCurrentPlayer()).toEqual("player1")
 });
 
 test('could get the owner after removing an even',function(){
-    let jeu = new Memory(['lune','soleil','lune','soleil']);
-    jeu.tabTilesObject[0].flip();
-    jeu.tabTilesObject[2].flip();
-    expect(jeu.tabTilesOwned[0].getOwner()).toEqual('player1')
+    let game = new Memory(['lune','soleil','lune','soleil']);
+    game.tabTilesObject[0].flip();
+    game.tabTilesObject[2].flip();
+    expect(game.tabTilesOwned[0].getOwner()).toEqual('player1')
 });
 
 test('get current player after removing an even',function(){
-    let jeu = new Memory(['lune','soleil','lune','soleil']);
-    jeu.tabTilesObject[0].flip();
-    jeu.tabTilesObject[2].flip();
-    expect(jeu.getCurrentPlayer()).toEqual("player2")
+    let game = new Memory(['lune','soleil','lune','soleil']);
+    game.tabTilesObject[0].flip();
+    game.tabTilesObject[2].flip();
+    expect(game.getCurrentPlayer()).toEqual("player2")
 });
 
 test('could get the owner after removing two even',function(){
-    let jeu = new Memory(['lune','soleil','lune','soleil']);
-    jeu.tabTilesObject[0].flip();
-    jeu.tabTilesObject[2].flip();
-    expect(jeu.tabTilesOwned[0].getOwner()).toEqual('player1')
-    jeu.tabTilesObject[0].flip();
-    jeu.tabTilesObject[1].flip();
-    expect(jeu.tabTilesOwned[2].getOwner()).toEqual('player2')
+    let game = new Memory(['lune','soleil','lune','soleil']);
+    game.tabTilesObject[0].flip();
+    game.tabTilesObject[2].flip();
+    expect(game.tabTilesOwned[0].getOwner()).toEqual('player1')
+    game.tabTilesObject[0].flip();
+    game.tabTilesObject[1].flip();
+    expect(game.tabTilesOwned[2].getOwner()).toEqual('player2')
 });
+
+/** does it work for a huge game? */
